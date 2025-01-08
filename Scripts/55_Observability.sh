8d3fb4c5425279684d7ffc48cf5e0b070bbef32c
cloudxabide <cloudxabide@gmail.com>
2025-01-08 10:56:04 -0500

    generic update

--- Scripts/55_Observability.sh
+++ Scripts/55_Observability.sh
@@ -34,6 +34,7 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm install $SERVICE grafana/grafana --namespace  $GRAFANA_NAMESPACE
 kubectl get secret --namespace $GRAFANA_NAMESPACE $SERVICE -o jsonpath="{.data.admin-password}" | base64 --decode > $GRAFANA_NAMESPACE-$SERVICE-secret.txt
+echo "$(cat monitoring-my-grafana-secret.txt)"
 
 DEFAULT_STORAGE_CLASS=$(kubectl get sc| grep "(default)" | awk '{ print $1 }')
 cat << EOF1 | tee $SERVICE-storage.yaml
