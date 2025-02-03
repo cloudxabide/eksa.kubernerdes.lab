4fcca62780c10d8eaf1130a12ca6986929993403
cloudxabide <cloudxabide@gmail.com>
2025-02-02 20:27:02 -0500

    re-adding vSphere

--- Files/var_www_html_services.php
+++ Files/var_www_html_services.php
@@ -102,3 +102,12 @@ echo "</pre></TD></TR>";
 echo "</TABLE>";
 
 ?>
+<BR>
+<TABLE BORDER=1>
+<TH colspan=3> Kubernerdes.Lab Infrastructure Services and Endpoints</TH>
+<TR><TD><font color=blue>Service</TD> <TD><font color=blue>Endpoint</TD></TR>
+<TR> <TD>vSphere Console</TD> <TD><A HREF="https://10.10.12.30/">https://10.10.12.30/</A></TD> </TR>
+<TR> <TD>ESXi Console (vmw-esx-01)</TD> <TD><A HREF="http://10.10.12.31">http://10.10.12.31</A></TD> </TR>
+<TR> <TD>
+</BODY>
+</HTML>
