1551f8b41801eccff52fac76311faba5cd406cf4
cloudxabide <cloudxabide@gmail.com>
2025-04-21 09:06:33 -0400

    swapping out placeholder VAR

--- Scripts/00_Post_Install_THEKUBERNERD.sh
+++ Scripts/00_Post_Install_THEKUBERNERD.sh
@@ -54,9 +54,9 @@ fi
 EOF
 
 #  Update my shell environment (optional)
-curl https://raw.githubusercontent.com/$GIT_USER/devops/main/Files/.bashrc.d_common | tee ~/.bashrc.d/common
-curl https://raw.githubusercontent.com/$GIT_USER/devops/main/Files/.bashrc.d_Ubuntu | tee ~/.bashrc.d/Ubuntu
-curl https://raw.githubusercontent.com/$GIT_USER/devops/main/Files/.bashrc.d_K8s | tee ~/.bashrc.d/K8s
+curl https://raw.githubusercontent.com/${GIT_USER}/devops/main/Files/.bashrc.d_common | tee ~/.bashrc.d/common
+curl https://raw.githubusercontent.com/${GIT_USER}/devops/main/Files/.bashrc.d_Ubuntu | tee ~/.bashrc.d/Ubuntu
+curl https://raw.githubusercontent.com/${GIT_USER}/devops/main/Files/.bashrc.d_K8s | tee ~/.bashrc.d/K8s
 . ~/.bashrc
 
 # Enable Firewall (if you use Ubuntu firewall)
@@ -106,9 +106,9 @@ install_desktop() {
 }
 
 # Create directories to clone this project repo to (for pull-only access - and this optional)
-mkdir -p $HOME/Repositories/Personal/$GIT_USER/; cd $_
-git clone https://github.com/$GIT_USER/eksa.kubernerdes.lab.git
-ln -s $HOME/Repositories/Personal/$GIT_USER/eksa.kubernerdes.lab $HOME
+mkdir -p $HOME/Repositories/Personal/${GIT_USER}/; cd $_
+git clone https://github.com/${GIT_USER}/eksa.kubernerdes.lab.git
+ln -s $HOME/Repositories/Personal/${GIT_USER}/eksa.kubernerdes.lab $HOME
 cd $HOME
 
 # Install Trivy (from Aquasecurity)
