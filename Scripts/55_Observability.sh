283e9f570d2d0d051efce5d904dbd52facf7d9bc
James Radtke <cloudxabide@gmail.com>
2025-08-20 10:01:57 -0400

    Moved K8s dashbaord to seperate script

--- Scripts/55_Observability.sh
+++ Scripts/55_Observability.sh
@@ -51,65 +51,6 @@ helm upgrade my-grafana grafana/grafana -f my-grafana-storage.yaml -n $GRAFANA_N
 cd -
 while sleep 2; echo; do kubectl get all -n monitoring | egrep 'ContainerCreating|Init' || break; done
 
-############### ############### ############### ###############
-## Kubernetes Dashbaord (WIP)
-# https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-kubectl -n kubernetes-dashboard patch svc kubernetes-dashboard-kong-proxy -p='{"spec": {"type": "LoadBalancer"}}'
-
-mkdir kubernetes-dashboard; cd $_
-
-# https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
-cat << EOF3 | tee kubernetes-dashboard-sa.yaml
----
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
-EOF3
-kubectl apply -f kubernetes-dashboard-sa.yaml
-
-cat << EOF5 | tee kubernetes-dashboard-clusterrolebinding.yaml
----
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: admin-user
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- kind: ServiceAccount
-  name: admin-user
-  namespace: kubernetes-dashboard
-EOF5
-kubectl apply -f kubernetes-dashboard-clusterrolebinding.yaml
-
-# kubectl -n kubernetes-dashboard create token admin-user
-cat << EOF6 | tee kubernetes-dashboard-sa-token.yaml
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
-  annotations:
-    kubernetes.io/service-account.name: "admin-user"   
-type: kubernetes.io/service-account-token  
-EOF6
-kubectl apply -f kubernetes-dashboard-sa-token.yaml
-
-kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d > kubernetes-dashboard-token.out
-echo $(kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} |  base64 -d)
-cd -
-
-# NOTE: THIS DOESN'T WORK AS MetalLB has not been enabled yet and therefore kong-proxy does not have a "public" IP
-#K8s_DASHBOARD=$(kubectl get svc kubernetes-dashboard-kong-proxy -n kubernetes-dashboard -o jsonpath='{.status.loadBalancer.ingress[].ip}')
-#echo -e "Access Kubernetes Dashboard at: \nhttps://$K8s_DASHBOARD"
-
 exit 0
 
 
